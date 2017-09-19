85b9c50c1f7673173ef0dff015c97deb6087c421
DESKTOP-5649448\DmitriyS <github@daserafin.com>
2017-09-19 17:14:03 +0300

    BT64 - [SOLVED] Using BlueprintReadOnly without wierd errors

--- BattleTank/Source/BattleTank/Private/Tank.cpp
+++ BattleTank/Source/BattleTank/Private/Tank.cpp
@@ -14,7 +14,7 @@ ATank::ATank()
 	// No need to protect points as added as construction
 	TankAimingComponent = CreateDefaultSubobject<UTankAimingComponent>(FName("Aiming Component"));
 	
-	//TankMovementComponent = CreateDefaultSubobject<UTankMovementComponent>(FName("Movement Component"));
+	TankMovementComponent = CreateDefaultSubobject<UTankMovementComponent>(FName("Movement Component"));
 }
 
 // Called when the game starts or when spawned
